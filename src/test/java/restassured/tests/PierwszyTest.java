ebb586629b79a505ee844fcb06289f54bd43dcaa
Norbert <norbert.bokwa@gmail.com>
2018-02-11 14:02:26 +0100

    First commit

---
+++ src/test/java/restassured/tests/PierwszyTest.java
@@ -0,0 +1,18 @@
+package restassured.tests;
+
+import org.testng.annotations.Test;
+
+public class PierwszyTest {
+
+
+    @Test
+    public void cos(){
+//        WebDriver driver = new ChromeDriver();
+        System.out.println("hello fuckers");
+    }
+
+    @Test
+    public void cos1(){
+        System.out.println("drugi test");
+    }
+}
